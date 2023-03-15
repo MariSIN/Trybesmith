668846ad371cad374204ed2883150f3825f34295
Mariana Nascimento <mariana.sn.silva@gmail.com>
2023-03-15 16:23:06 -0300

    [feat]: Cria middlewares para cadastrar usuários

---
+++ src/middlewares/user.ts
@@ -0,0 +1,105 @@
+import { NextFunction, Request, Response } from 'express';
+import statusCodes from '../statusCode';
+
+const validateUsername = (req: Request, res: Response, next: NextFunction) => {
+  const { username } = req.body;
+
+  if (!username) {
+    return res.status(statusCodes.BAD_REQUEST).json({
+      message: '"username" is required',
+    });
+  }
+
+  if (typeof username !== 'string') {
+    return res.status(statusCodes.UNPROCESSABLE_ENTITY).json({
+      message: '"username" must be a string',
+    });
+  }
+
+  if (username.length < 3) {
+    return res.status(statusCodes.UNPROCESSABLE_ENTITY).json({
+      message: '"username" length must be at least 3 characters long',
+    });
+  }
+
+  next();
+};
+
+const validateVocation = (req: Request, res: Response, next: NextFunction) => {
+  const { vocation } = req.body;
+
+  if (!vocation) {
+    return res.status(statusCodes.BAD_REQUEST).json({
+      message: '"vocation" is required',
+    });
+  }
+
+  if (typeof vocation !== 'string') {
+    return res.status(statusCodes.UNPROCESSABLE_ENTITY).json({
+      message: '"vocation" must be a string',
+    });
+  }
+
+  if (vocation.length < 3) {
+    return res.status(statusCodes.UNPROCESSABLE_ENTITY).json({
+      message: '"vocation" length must be at least 3 characters long',
+    });
+  }
+
+  next();
+};
+
+const validateLevel = (req: Request, res: Response, next: NextFunction) => {
+  const { level } = req.body;
+  
+  if (level === undefined) {
+    return res.status(statusCodes.BAD_REQUEST).json({
+      message: '"level" is required',
+    });
+  }
+  
+  if (typeof level !== 'number') {
+    return res.status(statusCodes.UNPROCESSABLE_ENTITY).json({
+      message: '"level" must be a number',
+    });
+  }
+  
+  if (level <= 0) {
+    return res.status(statusCodes.UNPROCESSABLE_ENTITY).json({
+      message: '"level" must be greater than or equal to 1',
+    });
+  }
+  
+  next();
+};
+
+const validatePassword = (req: Request, res: Response, next: NextFunction) => {
+  const { password } = req.body;
+  
+  if (!password) {
+    return res.status(statusCodes.BAD_REQUEST).json({
+      message: '"password" is required',
+    });
+  }
+  
+  if (typeof password !== 'string') {
+    return res.status(statusCodes.UNPROCESSABLE_ENTITY).json({
+      message: '"password" must be a string',
+    });
+  }
+  
+  if (password.length < 8) {
+    return res.status(statusCodes.UNPROCESSABLE_ENTITY).json({
+      message: '"password" length must be at least 8 characters long',
+    });
+  }
+  
+  next();
+};
+
+export default {
+  validateUsername,
+  validateVocation,
+  validateLevel,
+  validatePassword,
+};
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,8 +1,16 @@
 import { Router } from 'express';
 import userController from '../controllers/user.controller';
+import user from '../middlewares/user';
 
 const userRouter = Router();
 
-userRouter.post('/', userController.create);
+userRouter.post(
+  '/',
+  user.validateUsername, 
+  user.validateVocation,
+  user.validateLevel,
+  user.validatePassword,
+  userController.create,
+);
 
 export default userRouter;
